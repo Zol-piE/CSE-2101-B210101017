d257646fb8442507d1617c7c399a3e05d0e6b601
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-01-02 23:22:39 +0600

    Update insertionSortLinkedList.py

--- Sorting Algo/insertionSortLinkedList.py
+++ Sorting Algo/insertionSortLinkedList.py
@@ -13,12 +13,13 @@ def intialize():
 def insertionSort():
     global number
     for i in range(1,n):
-        key = number[i]
         j = i-1
-        while j>=0 and number[j]>key:
+        key = number[i]
+        while j>=0 and number[j] > key:
             number[j+1] = number[j]
             j-=1
-        number[j+1] = key
+        number[j+1] = key  
+
 
 intialize()
 print(number)
